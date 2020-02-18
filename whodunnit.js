2309fad19726cb1213b1c31375112c830cd7c922
Ruiner85 <paul.m.mackay85@gmail.com>
2020-02-18 09:02:52 +0000

    initial commit

---
+++ whodunnit.js
@@ -0,0 +1,182 @@
+// // Episode 1
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Library',
+//   weapon: 'Rope'
+// };
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${scenario.murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
+//
+// // Expected output - "The murderer is Miss Scarlet".  console.log outputs the value of verdict. verdict receives it's value from the result of declareMurderer. declareMurderer has access to scenario from the outer scope. Scenario at the outer most scope defines the murderer.
+//
+// // Episode 2
+// const murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+//
+// // Expected output - Error. You can't change a constant after it has already been declared (line 21).
+//
+// // Episode 3
+// let murderer = 'Professor Plum';
+//
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+//
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+//
+// // Expected outcome - "First Verdict: The murderer is Mrs. Peacock." "Second Verdict: The murderer is Professor Plum." The string "Mrs. Peacock" is only accessable within the function assigned to declareMurderer. Whereas "Professor Plum" is avaiable outwith that function, so must be the murderer in secondVerdict.
+//
+// // Episode 4
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+//
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+//
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
+//
+// // Expected outcome - "The suspects are Miss Scarlet, Professor Plum, Colonel Mustard." "Suspect three is Mrs. Peacock." Colonel Mustard is only accessable as the third suspect within the declareAllSuspects function, whereas Mrs. Peacock is avaiable globally.
+//
+// // Episode 5
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Kitchen',
+//   weapon: 'Candle Stick'
+// };
+//
+// const changeWeapon = function(newWeapon) {
+//   scenario.weapon = newWeapon;
+// }
+//
+// const declareWeapon = function() {
+//   return `The weapon is the ${scenario.weapon}.`;
+// }
+//
+// changeWeapon('Revolver');
+// const verdict = declareWeapon();
+// console.log(verdict);
+//
+// // Expected outcome - "The weapon is the Revolver." The function changeWeapon sets a new instance of scenario.weapon from the string "Candle Stick" to "Revolver".
+//
+// // Episode 6
+// let murderer = 'Colonel Mustard';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+//
+//   const plotTwist = function() {
+//     murderer = 'Mrs. White';
+//   }
+//
+//   plotTwist();
+// }
+//
+// const declareMurderer = function () {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+//
+// // Expected outcome - "The murderer is Mrs. White.". The murderer is changed to Mrs. White within the block by plotTwist. changeMurderer is declared outside the block so cannot be accessed.
+//
+// // Episode 7
+// let murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+//
+//   const plotTwist = function() {
+//     let murderer = 'Colonel Mustard';
+//
+//     const unexpectedOutcome = function() {
+//       murderer = 'Miss Scarlet';
+//     }
+//
+//     unexpectedOutcome();
+//   }
+//
+//   plotTwist();
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+//
+// // Expected outcome - "The murderer is Mr. Green." changeMurderer is called last outside the block, so the answer should be Mr. Green.
+//
+// // Episode 8
+// let murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+//
+//   const plotTwist = function() {
+//     let murderer = 'Colonel Mustard';
+//
+//     const unexpectedOutcome = function() {
+//       murderer = 'Miss Scarlet';
+//     }
+//
+//     unexpectedOutcome();
+//   }
+//
+//   plotTwist();
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+//
+// // Expected outcome - "The murderer is Mr. Green.". The changeMurderer function is called last outside of the block so should change the outcome to Mr. Green.
+//
+// // Episode 9
+// let murderer = 'Professor Plum';
+//
+// if (murderer === 'Professor Plum') {
+//   let murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
+//
+// // Expected outcome - "The murderer is Professor Plum.". The let cannot escape the block, therefore the murderer will not be changed.
